67d616280e1a0ac5e5c7c54f9659ab27bc36c6d7
Vincent Le Goualher <vlg.engineer@gmail.com>
2025-06-01 17:03:49 +0000

    Complete check pytorch

--- dev/check_pytorch_install.py
+++ dev/check_pytorch_install.py
@@ -1,5 +1,5 @@
 import torch
-print(torch.cuda.is_available())
-print(torch.cuda.current_device())
-print(torch.cuda.get_device_name(0))
-print(torch.version.cuda)
+print(f"{torch.cuda.is_available() = }")
+print(f"{torch.cuda.current_device() = }")
+print(f"{torch.cuda.get_device_name(0) = }")
+print(f"{torch.version.cuda = }")

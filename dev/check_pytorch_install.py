f61ef3c3d76dc961d07c59b5da62d8f9b1ecb252
Vincent Le Goualher <v.legoualher@gmail.com>
2025-06-08 16:12:48 +0200

    Init project structure

--- dev/check_pytorch_install.py
+++
@@ -1,5 +0,0 @@
-import torch
-print(f"{torch.cuda.is_available() = }")
-print(f"{torch.cuda.current_device() = }")
-print(f"{torch.cuda.get_device_name(0) = }")
-print(f"{torch.version.cuda = }")
---
+++ model/deploy_model.sh
@@ -0,0 +1,23 @@
+gcloud run deploy llm-server \
+   --image us-docker.pkg.dev/cloudrun/container/gemma/gemma3-4b \
+   --cpu 8 \
+   --concurrency 4 \
+   --set-env-vars "OLLAMA_NUM_PARALLEL=4" \
+   --set-env-vars="API_KEY=<API_KEY>" \
+   --gpu 1 \
+   --gpu-type nvidia-l4 \
+   --max-instances 1 \
+   --memory 32Gi \
+   --allow-unauthenticated \
+   --no-cpu-throttling \
+   --timeout=600 \
+   --region europe-west1
+
+curl "<CLOUD_RUN_URL>/v1beta/models/gemma-3-4b-it:generateContent?key=<API_KEY>" \
+   -H 'Content-Type: application/json' \
+   -X POST \
+   -d '{
+     "contents": [{
+       "parts":[{"text": "Hi, can you tell me a fun fact about Switzerland?"}]
+       }]
+      }'

2c0b414a56783526986b7860288b0e0f38239198
Vincent Le Goualher <v.legoualher@gmail.com>
2025-07-05 16:50:19 +0000

    Deploy Spring backend on Cloud Run

---
+++ deploy/cloud_run.sh
@@ -0,0 +1,17 @@
+# Firestore permission
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" \
+  --role="roles/datastore.user"
+
+# Cloud run
+gcloud run deploy llm-backend \
+  --image=europe-west6-docker.pkg.dev/llm-server-461514/docker-repo/backend:latest \
+  --platform=managed \
+  --region=europe-west6 \
+  --allow-unauthenticated \
+  --port=8080 \
+  --set-env-vars="API_KEY=$API_KEY,CLOUD_RUN_URL=$CLOUD_RUN_URL" \
+  --memory=1Gi \
+  --cpu=1 \
+  --min-instances=0 \
+  --max-instances=1

2fba5c3c7ce4ea9eeb10a63391b96567b8c981ae
Vincent Le Goualher <v.legoualher@gmail.com>
2025-07-05 14:18:11 +0000

    Use gradle to build container instead of docker

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,7 @@
 # gcloud commands
 gcloud init
 gcloud auth application-default login
+
+# gradle
+./gradlew clean build --refresh-dependencies
+./gradlew bootBuildImage --imageName=gcr.io/my-project/myapp:latest
